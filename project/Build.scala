fedb9b9760d8f71478537039d97d3d8c2abd7a8a
Morgen Peschke <morgen.peschke@gmail.com>
2017-01-28 15:05:50 -0800

    GitIgnore and Build.scala

---
+++ project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+
+object BuildScalaGlob extends Build {
+  val commonDependencies = Seq(
+    "com.lihaoyi" %% "fastparse" % "0.4.1",
+    "org.scalatest"  %% "scalatest" % "2.2.2" % "test",
+    "org.scalamock"  %% "scalamock-scalatest-support" % "3.2" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.12.5" % "test")
+
+  val commonSettings = Seq(
+    version := "0.0",
+    scalaVersion := "2.11.7",
+    libraryDependencies ++= commonDependencies,
+    scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"))
+
+  lazy val scalaGlob = project
+    .in(file("."))
+    .settings(commonSettings)
+}
